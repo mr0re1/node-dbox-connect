2d14789a99b6be8e43698e0fa74a7c8899612e6c
Ivan Orlov <camarada.0re1@gmail.com>
2013-06-05 22:09:55 +0400

    Add upload and uploader methods

--- test/all.js
+++ test/all.js
@@ -1,18 +1,84 @@
 var connect = require('connect')
   , request = require('supertest')
+  , should = require('should')
   , fs = require('fs')
   , DBoxConnect = require('..');
 
 
-describe('DBoxExpress', function(){
-  var dbox_connect = new DBoxConnect(require('./conf/token'))
+describe('all', function(){
+  try {
+    var token = require('./conf/token');
+  } catch (e) {
+    console.log('To generate token use \'npm dbox-connect start\'');
+    return;
+  }
+  var dbox_connect = new DBoxConnect(token)
     , app = connect();
     
   app.use(dbox_connect.redirect('/test'));
+ 
   
-  describe('upload', function() {
-    it('should upload file')
+  var dir_name = 'mydir';
+
+  before(function(done){
+    dbox_connect.rm(dir_name, function() { done() });
+  });
+
+    
+  describe('mkdir', function() {
+    it('should make directory', function(done) {
+      this.timeout(5000);
+
+      dbox_connect.mkdir(dir_name, function(err, reply) {
+        should.not.exist(err);
+        should.exist(reply);
+        reply.should.have.property('path', '/' + dir_name);
+        done();
+      });
+    })
+
+    it('should return error when folder already exist', function(done){
+      this.timeout(5000);
+
+      dbox_connect.mkdir(dir_name, function(err, reply){
+        should.exist(err);
+        should.not.exist(reply);
+        err.should.have.property('error');
+        err.error.should.equal(
+          ' at path \'The folder \''+dir_name+'\' already exists.\'');
+        done();
+      })
+    });
   })
+
+
+  describe('upload', function() {
+    it('should upload file', function(done) {
+      this.timeout(5000);
+
+      dbox_connect.upload(
+        './test/sample/hello.txt', 
+        dir_name + '/hello.txt',
+        function(err, reply){
+          should.not.exist(err);
+          should.exist(reply);
+          reply.should.have.property('path', '/' + dir_name + '/hello.txt');
+          done()
+        })
+    });
+  });
+
+  describe('rm', function(){
+    it('should remove directory', function(done){
+      this.timeout(5000);
+
+      dbox_connect.rm(dir_name, function(err, reply){  
+        should.not.exist(err);
+        reply.should.have.property("path", '/' + dir_name);
+        done();
+      })
+    });
+  });
 
 });
 
