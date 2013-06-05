0939835e98144d800c3093884d776aa5eeb9fe32
Ivan Orlov <camarada.0re1@gmail.com>
2013-06-05 23:18:09 +0400

    Add get method

--- test/all.js
+++ test/all.js
@@ -18,7 +18,11 @@ describe('all', function(){
   app.use(dbox_connect.redirect('/test'));
  
   
-  var dir_name = 'mydir';
+  var dir_name = 'mydir'
+    , file_name = 'hello.txt'
+    , local_file ='./test/sample/' + file_name
+    , remote_file = '/' + dir_name + '/' + file_name
+    , file_content = fs.readFileSync(local_file).toString();
 
   before(function(done){
     dbox_connect.rm(dir_name, function() { done() });
@@ -43,29 +47,91 @@ describe('all', function(){
       dbox_connect.mkdir(dir_name, function(err, reply){
         should.exist(err);
         should.not.exist(reply);
-        err.should.have.property('error');
-        err.error.should.equal(
-          ' at path \'The folder \''+dir_name+'\' already exists.\'');
+        err.should.equal(' at path \'The folder \''+dir_name+'\' already exists.\'');
         done();
       })
     });
   })
 
 
-  describe('upload', function() {
+  describe('put', function() {
     it('should upload file', function(done) {
       this.timeout(5000);
 
-      dbox_connect.upload(
-        './test/sample/hello.txt', 
-        dir_name + '/hello.txt',
+      dbox_connect.put(
+        local_file, 
+        remote_file,
         function(err, reply){
           should.not.exist(err);
           should.exist(reply);
-          reply.should.have.property('path', '/' + dir_name + '/hello.txt');
+          reply.should.have.property('path', remote_file);
           done()
         })
     });
+
+    it('should return error', function(done) {
+      this.timeout(5000);
+
+      dbox_connect.put(
+        local_file + '_invalid', 
+        dir_name + '/hello.txt',
+        function(err, reply){
+          should.exist(err);
+          should.not.exist(reply);
+          done()
+        })
+    });
+
+  });
+  
+  describe('get', function(){
+    
+    it('should get file content', function(done) {
+      this.timeout(5000);
+
+      dbox_connect.get(remote_file, function(err, content, metadata){
+        should.not.exist(err);
+        should.exist(content);
+        should.exist(metadata);
+        metadata.should.have.property('path', remote_file);
+        content.toString().should.equal(file_content);
+        done();
+      });
+    })
+    
+    it('should return error', function(done){
+      this.timeout(5000);
+
+      dbox_connect.get(remote_file + 'invalid', function(err, content, metadata){
+        should.exist(err);
+        should.not.exist(content);
+        should.not.exist(metadata);
+        done();
+      });
+    })
+
+  })
+
+  describe('getDirectLink', function(){
+    it('should return url', function(done){
+      this.timeout(5000);
+      
+      dbox_connect.getDirectLink(remote_file, function(err, url){
+        should.not.exist(err);
+        should.exist(url);
+        done();
+      });
+    })
+
+    it('should return url faster', function(done){
+      this.timeout(100);
+
+      dbox_connect.getDirectLink(remote_file, function(err, url){
+        should.not.exist(err);
+        should.exist(url);
+        done();
+      });
+    })
   });
 
   describe('rm', function(){
